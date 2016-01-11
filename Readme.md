60de1d33cf8e54625417416f8a209c071b9fbd03
Alexey Karasev <karasev.alexey@gmail.com>
2016-01-11 12:14:56 +0500

    initial commit

---
+++ Readme.md
@@ -0,0 +1,7 @@
+## Usage
+
+`webpack` - to start development mode
+
+`DEBUG=t webpack` - to start debug mode (use DEBUG constant in code)
+
+`NODE_ENV=production webpack` to build for production
---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+var webpack = require('webpack');
+var path = require('path');
+const NODE_ENV = process.env.NODE_ENV || "development";
+const DEBUG = process.env.DEBUG ? true : false;
+const DEVELOPMENT = NODE_ENV == 'development';
+
+var config = {
+    devtool: DEVELOPMENT ? 'cheap-module-source-map' : null,
+    entry: {
+        app: './src/app'
+    },
+    output: {
+        path: path.join(__dirname, DEVELOPMENT ? 'public' : 'dist'),
+        filename: 'ru-upfinder-silk.js'
+    },
+    plugins: [
+        new webpack.NoErrorsPlugin(),
+        new webpack.DefinePlugin({
+            DEVELOPMENT: DEVELOPMENT,
+            DEBUG: DEBUG
+        })
+        //new webpack.ProvidePlugin({
+        //    '_': 'lodash',
+        //    'babel-polyfill': 'babel-polyfill'
+        //})
+    ],
+    resolve: {
+        extensions: ['', '.js']
+    },
+
+    watch: DEVELOPMENT,
+
+    watchOptions: {
+      aggregateTimeout: 100
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                include: [
+                    path.resolve(__dirname, "src")
+                ],
+                loader: 'babel-loader',
+                query: {
+                    plugins: ['transform-runtime'],
+                    presets: ['es2015'],
+                    cacheDirectory: true
+                }
+            }
+        ]
+    }
+};
+
+uglifyPlugin = new webpack.optimize.UglifyJsPlugin({
+    compress: {
+        warnings: false
+    }
+});
+
+if (!DEVELOPMENT) {
+    config.plugins.push(uglifyPlugin);
+}
+
+module.exports = config;
