d7d8c771b356457c4bca7ee68ce0d5139c0f3e0b
Andrew <block159@gmail.com>
2016-02-08 12:09:00 +0300

    deploy script

---
+++ bin/deploy.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+scp dist/ru-upfinder-silk.js deploy@upfinder.ru:apps/production/landing/js/ru-upfinder-silk.js
--- start.js
+++
@@ -1,2 +0,0 @@
-http-server sandbox -a localhost -p 8000 -c-1
-open localhost:8000
